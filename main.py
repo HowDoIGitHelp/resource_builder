a90096bd8323ff416448cfc4adafa45bc341afa6
Rubelito Abella <bbadoodles@gmail.com>
2026-01-24 23:36:11 +0800

    add preamble and postable for remark.js generation

--- main.py
+++ main.py
@@ -9,11 +9,20 @@ def main():
     file = open('Counting and Discrete Probability.md', 'r')
     processedMD = preprocess(open('Counting and Discrete Probability.md', 'r'))
     currentHead = None
+    preamble = open('slidesPre.html','r').read()
+    postamble = open('slidesPost.html','r').read()
+    output = open('slides.html', 'w+')
+    output.write(preamble)
+
     for child in Document(file.read()).children:
         if isinstance(child, Heading):
             currentHead = Head(child)
+            if currentHead.level() < 4:
+                output.write(currentHead.mdSlides())
         else:
-            print(asBlock(child).mdSlides(head = currentHead))
+            output.write(asBlock(child, verbose = False).mdSlides(head = currentHead))
+
+    output.write(postamble)
 
 if __name__ == "__main__":
     main()
